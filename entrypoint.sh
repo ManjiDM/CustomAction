fcaa61a3a3d9d70e50900a3a13f48d9c4842b049
Daniel Mota <manji.dm@gmail.com>
2024-05-03 21:27:25 +0100

    teste

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,11 +22,11 @@ then
     # do something
     VERSION=$(date +%F.%s)
 
-    DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
-    DATA="${DATA} $(printf '"target_commitish":"master",')"
-    DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
-    DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
-    DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
+    # DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
+    # DATA="${DATA} $(printf '"target_commitish":"master",')"
+    # DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
+    # DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
+    # DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
 
     URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
 
